bc44abdbf1a3d15f1146081fa8be005cde477414
ozzy <ozzycharon@gmail.com>
2024-06-19 01:20:40 +0800

    build: add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import pathlib
+
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='code-simplify',
+    version='1.0.0',
+    description='A python tool library that covers a wide range of common tools to simplify your work and reduce '
+                'repetitive code.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yaocool/code-simplify",
+    keywords="python, http utils, logger utils",
+    author='Ozzy',
+    author_email='ozzycharon@gmail.com',
+    license='MIT',
+    install_requires=['aiohttp'],
+    packages=find_packages(exclude=['examples']),
+)
